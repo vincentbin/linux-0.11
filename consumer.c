4e0f258b239ab8fb45367a5556b5e8a0fd8bb954
vincentAffinity <yanyibin@jmgo.com>
2021-12-22 11:12:35 +0800

    sem

--- consumer.c
+++ consumer.c
@@ -1,4 +1,3 @@
-/*consumer.c*/
 #define   __LIBRARY__
 #include <unistd.h>
 #include <sys/types.h>
@@ -14,17 +13,37 @@ _syscall1(int,sem_unlink,const char *,name);
 _syscall1(void*,shmat,int,shmid);
 _syscall2(int,shmget,int,key,int,size);
 
-#define NUMBER 5 /*打出数字总数*/
+
+#define NUMBER 520 /*打出数字总数*/
 #define BUFSIZE 10 /*缓冲区大小*/
+
+sem_t *empty, *full, *mutex;
 
 int main()
 {
-    int  i,shmid,data;
+    int  i, shmid, data;
     int *p;
     int  buf_out = 0; /*从缓冲区读取位置*/
+    /*打开信号量*/
+    if((mutex = sem_open("mutex", 1)) == NULL)
+    {
+        perror("sem_open() error!\n");
+        return -1;
+    }
+    if((empty = sem_open("empty", 10)) == NULL)
+    {
+        perror("sem_open() error!\n");
+        return -1;
+    }
+    if((full = sem_open("full", 0)) == NULL)
+    {
+        perror("sem_open() error!\n");
+        return -1;
+    }
 
     printf("consumer start.\n");
     fflush(stdout);
+    /*shmid = shmget("buffer");*/
     shmid = shmget(1234, BUFSIZE);
     printf("shmid:%d\n",shmid);
     if(shmid == -1)
@@ -33,13 +52,26 @@ int main()
     }
     p = (int *) shmat(shmid);
 
-    for( i = 0; i < NUMBER; i++ ) {
+    for( i = 0; i < NUMBER; i++ )
+    {
+        sem_wait(full);
+        sem_wait(mutex);
+
         data = p[buf_out];
         buf_out = (buf_out + 1) % BUFSIZE;
-        printf("%d: %d.\n", getpid(), data);
+
+        sem_post(mutex);
+        sem_post(empty);
+        /*消费资源*/
+        printf("%d:  %d\n",getpid(),data);
         fflush(stdout);
     }
     printf("consumer end.\n");
     fflush(stdout);
+    /*释放信号量*/
+    sem_unlink("full");
+    sem_unlink("empty");
+    sem_unlink("mutex");
+
     return 0;
 }
--- producer.c
+++ producer.c
@@ -1,4 +1,3 @@
-/*producer.c*/
 #define   __LIBRARY__
 #include <unistd.h>
 #include <sys/types.h>
@@ -13,19 +12,35 @@ _syscall1(int,sem_post,sem_t*,sem);
 _syscall1(int,sem_unlink,const char *,name);
 _syscall1(void*,shmat,int,shmid);
 _syscall2(int,shmget,int,key,int,size);
-/*_syscall1(int,shmget,char*,name);*/
 
-#define NUMBER 5 /*打出数字总数*/
+#define NUMBER 520 /*打出数字总数*/
 #define BUFSIZE 10 /*缓冲区大小*/
 
+sem_t   *empty, *full, *mutex;
 int main()
 {
     int  i, shmid;
     int *p;
     int  buf_in = 0; /*写入缓冲区位置*/
+    /*打开信号量*/
+    if((mutex = sem_open("mutex", 1)) == NULL)
+    {
+        perror("sem_open() error!\n");
+        return -1;
+    }
+    if((empty = sem_open("empty", 10)) == NULL)
+    {
+        perror("sem_open() error!\n");
+        return -1;
+    }
+    if((full = sem_open("full", 0)) == NULL)
+    {
+        perror("sem_open() error!\n");
+        return -1;
+    }
     /*shmid = shmget("buffer");*/
     shmid = shmget(1234, BUFSIZE);
-    printf("shmid: %d.\n", shmid);
+    printf("shmid:%d\n",shmid);
     if(shmid == -1)
     {
         return -1;
@@ -34,12 +49,20 @@ int main()
     /*生产者进程*/
     printf("producer start.\n");
     fflush(stdout);
-    for(i = 0 ; i < NUMBER; i++) {
-        printf("%d.\n", i);
+    for( i = 0 ; i < NUMBER; i++)
+    {
+        sem_wait(empty);
+        sem_wait(mutex);
         p[buf_in] = i;
-        buf_in = (buf_in + 1) % BUFSIZE;
+        buf_in = ( buf_in + 1)% BUFSIZE;
+        sem_post(mutex);
+        sem_post(full);
     }
     printf("producer end.\n");
     fflush(stdout);
+    /*释放信号量*/
+    sem_unlink("full");
+    sem_unlink("empty");
+    sem_unlink("mutex");
     return 0;
 }
