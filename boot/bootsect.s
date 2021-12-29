49dffc1d35307aee15c6aeaa1332292c7483452e
vincentAffinity <yanyibin@jmgo.com>
2021-12-29 23:48:08 +0800

    print

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -95,7 +95,7 @@ ok_load_setup:
 	xor	bh,bh
 	int	0x10
 	
-	mov	cx,#338
+	mov	cx,#36
 	mov	bx,#0x0007		! page 0, attribute 7 (normal)
 	mov	bp,#msg1
 	mov	ax,#0x1301		! write string, move cursor
@@ -242,18 +242,8 @@ sectors:
 	.word 0
 
 msg1:
-	.byte 13,10
-    .ascii "#    #     #    #    #   ####   ######  #    #   #####"
     .byte 13,10
-    .ascii "#    #     #    ##   #  #    #  #       ##   #     #"
-    .byte 13,10
-    .ascii "#    #     #    # #  #  #       #####   # #  #     #"
-    .byte 13,10
-    .ascii "#    #     #    #  # #  #       #       #  # #     #"
-    .byte 13,10
-    .ascii " #  #      #    #   ##  #    #  #       #   ##     #"
-    .byte 13,10
-    .ascii "  ##       #    #    #   ####   ######  #    #     #"
+    .ascii "Hello OS world, my name is YYB"
 	.byte 13,10,13,10
 
 .org 508
